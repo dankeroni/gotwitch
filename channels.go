6bfd85734dc85331ad81d0f64b009022b82436e7
dankeroni <pana99@hotmail.de>
2016-07-31 22:56:20 +0200

    Added /channel and /channels/:channel

--- channels.go
+++ channels.go
@@ -25,16 +25,59 @@ type Channel struct {
 	URL                          string      `json:"url"`
 	Views                        int         `json:"views"`
 	Followers                    int         `json:"followers"`
-	Links                        struct {
-		Self          string `json:"self"`
-		Follows       string `json:"follows"`
-		Commercial    string `json:"commercial"`
-		StreamKey     string `json:"stream_key"`
-		Chat          string `json:"chat"`
-		Features      string `json:"features"`
-		Subscriptions string `json:"subscriptions"`
-		Editors       string `json:"editors"`
-		Teams         string `json:"teams"`
-		Videos        string `json:"videos"`
-	} `json:"_links"`
+}
+
+type AuthenticatedChannel struct {
+	Mature                       bool        `json:"mature"`
+	Status                       string      `json:"status"`
+	BroadcasterLanguage          string      `json:"broadcaster_language"`
+	DisplayName                  string      `json:"display_name"`
+	Game                         string      `json:"game"`
+	Delay                        interface{} `json:"delay"`
+	Language                     string      `json:"language"`
+	ID                           int         `json:"_id"`
+	Name                         string      `json:"name"`
+	CreatedAt                    time.Time   `json:"created_at"`
+	UpdatedAt                    time.Time   `json:"updated_at"`
+	Logo                         string      `json:"logo"`
+	Banner                       string      `json:"banner"`
+	VideoBanner                  string      `json:"video_banner"`
+	Background                   interface{} `json:"background"`
+	ProfileBanner                string      `json:"profile_banner"`
+	ProfileBannerBackgroundColor string      `json:"profile_banner_background_color"`
+	Partner                      bool        `json:"partner"`
+	URL                          string      `json:"url"`
+	Views                        int         `json:"views"`
+	Followers                    int         `json:"followers"`
+	Email                        string      `json:"email"`
+	StreamKey                    string      `json:"stream_key"`
+}
+
+// ChannelSuccessCallback runs on a successfull request and parse using the Stream method
+type ChannelSuccessCallback func(Channel)
+
+// AuthenticatedChannelSuccessCallback runs on a successfull request and parse using the Stream method
+type AuthenticatedChannelSuccessCallback func(AuthenticatedChannel)
+
+// Channel request for https://api.twitch.tv/kraken/channels/:channel
+func (twitchAPI *TwitchAPI) Channel(channelName string, onSuccess ChannelSuccessCallback,
+	onHTTPError HTTPErrorCallback, onInternalError InternalErrorCallback) {
+	var channel Channel
+	onSuccessfulRequest := func() {
+		onSuccess(channel)
+	}
+	twitchAPI.Get("/channels/"+channelName, nil, "", &channel, onSuccessfulRequest,
+		onHTTPError, onInternalError)
+}
+
+// AuthenticatedChannel request for https://api.twitch.tv/kraken/channel
+func (twitchAPI *TwitchAPI) AuthenticatedChannel(oauthToken string,
+	onSuccess AuthenticatedChannelSuccessCallback, onHTTPError HTTPErrorCallback,
+	onInternalError InternalErrorCallback) {
+	var authenticatedChannel AuthenticatedChannel
+	onSuccessfulRequest := func() {
+		onSuccess(authenticatedChannel)
+	}
+	twitchAPI.Get("/channel", nil, oauthToken, &authenticatedChannel, onSuccessfulRequest,
+		onHTTPError, onInternalError)
 }
