9f2719ea9d613ec1af71c4be35fcddd0e03511ab
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-02-20 16:26:21 +0100

    remove internal output from http/internal errors

--- common.go
+++ common.go
@@ -6,17 +6,13 @@ import (
 
 func simpleOnHTTPError(channel chan error) func(code int, statusMessage, errorMessage string) {
 	return func(code int, statusMessage, errorMessage string) {
-		fmt.Println("ERROR")
 		channel <- fmt.Errorf("HTTP Error %d: %s - %s", code, statusMessage, errorMessage)
-		fmt.Println("lol")
 	}
 }
 
 func simpleOnInternalError(channel chan error) func(err error) {
 	return func(err error) {
-		fmt.Println("INTERNAL ERROR")
 		channel <- err
-		fmt.Println("lol")
 	}
 }
 
